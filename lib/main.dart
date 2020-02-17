a3b44e124d919b36d5c3b829ab1663cc0216dcde
Lucas Bento <lbento@tyro.com>
2020-02-17 15:26:20 +1100

    Add backdrop widget to main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apod_daydream_flutter/backdrop/backdrop_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,55 +10,13 @@ class MyApp extends StatelessWidget {
       title: 'APOD Daydream',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 34.0, fontFamily: 'Roboto', color: Colors.white),
+          headline2: TextStyle(fontSize: 16.0, fontFamily: 'Roboto', color: Colors.white70),
+          headline3: TextStyle(fontSize: 12.0, fontFamily: 'Roboto', color: Colors.white70),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
+      home: BackdropPage(), //<--- Our backdrop page
     );
   }
 }
